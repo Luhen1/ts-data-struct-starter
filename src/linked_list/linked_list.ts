e0f4bffbbeb0b571c58dc551463dd4ae390c58fb
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2021-12-27 14:30:08 -0300

    Linked list done

--- src/linked_list/linked_list.ts
+++ src/linked_list/linked_list.ts
@@ -11,31 +11,101 @@ export class LinkedList<T> {
     }
 
     public isEmpty(): boolean {
-        throw new Error('Method not implemented');
+        return this.head.next === this.tail;
     }
 
     public insertFirst(item: T): void {
-        throw new Error('Method not implemented');
+        // encapsulate our item into a node object
+        const newNode = new DS.Node<T>(item);
+
+        // first we put the box 1´s next to the tail. and linking it to the new box. the next of the new box is now linked to the tail 
+        newNode.next = this.head.next;
+        // then we get the next of the box 1 and link it to the new box. 
+        this.head.next = newNode;
     }
 
     public insertLast(item: T): void {
+        const newNode = new DS.Node<T>(item)
 
+        let cur: DS.Node<T> | null = this.head;
+
+        //advance our cur pointer to just before the tail node 
+        while (cur && cur.next !== this.tail) {
+            cur = cur.next;
+        }
+
+        if (cur) {
+            newNode.next = this.tail;
+            cur.next = newNode;
+        }
     }
 
     public removeFirst(): T | null {
-        throw new Error('Method not implemented');
+        if (this.isEmpty()){
+            throw new Error("List is empty");
+        }
+
+        let rv: DS.Node<T> | null = this.head.next;
+
+        if (rv) {
+            this.head.next = rv.next;
+            rv.next = null;
+        }
+
+        //we are returning the data, not the node itself
+        return rv ? rv.item : null;
     }
 
     public remove(searchKey: T): T | null {
-        throw new Error('Method not implemented');
+        if (this.isEmpty()) {
+            throw new Error('List is empty');
+        }
+
+        // rv = retval or return value
+        let rv: DS.Node<T> | null = null;
+
+        // cur = current
+        let cur: DS.Node<T> = this.head;
+
+        // Advance our cur pointer to the node right before our matching node
+        while (cur.next && cur.next.item !== searchKey) {
+            cur = cur.next;
+        }
+
+        if (cur.next) {
+            rv = cur.next;
+            cur.next = cur.next.next;
+            rv.next = null;
+        }
+
+        return rv && rv.item ? rv.item : null;
     }
 
     public contains(searchItem: T): boolean {
-        throw new Error('Method not implemented');
+        if (this.isEmpty()){
+            throw new Error('List is empty');
+        }
+
+        let rv: boolean = false;
+        let cur: DS.Node<T> | null = this.head;
+
+        // Traverse the list in search of a matching item
+        while (cur && cur.next !== this.tail) {
+            if (cur.next && cur.next.item === searchItem) {
+                rv = true;
+                break;
+            }
+            cur = cur.next;
+        }
+        return rv;
     }
 
     public getFirst(): T | null {
-        throw new Error('Method not implemented');
+        if (this.isEmpty()){
+            throw new Error('List is empty');
+        }
+
+        return this.head.next ? this.head.next.item : null;
     }
 
     public listContents(): void {
--- src/linked_list/node.ts
+++ src/linked_list/node.ts
@@ -1,5 +1,5 @@
 // DS = Data Structure
-export namespace DS {
+export namespace DS { //namespace to ensure that this variable is not global but local. 
     export class Node<T> {
         public item: T | null;
         public next: Node<T> | null;
@@ -10,3 +10,11 @@ export namespace DS {
         }
     }
 }
+
+//Both item and next are public properties. 
+//Usually, properties within a class are private or protected, 
+//with getters and setters providing access to the properties. 
+//Here, we are making an exception. 
+//A linked list can have millions of nodes, 
+//and the overhead of using getters and setters will be excessive. 
+//It is better to give direct access to these properties to enhance performance.
