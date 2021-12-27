e270e1d8a9823d5fea5d8ac9182a6458a8fa8e3d
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2021-12-27 10:47:38 -0300

    Completed Queue exercise

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -1,5 +1,7 @@
-export class Queue {
-    // private queue: [];
+export class Queue<T> { //generics which can be inserted a type that the queue will be using
+    // example: if i put the first element as a number. it will be a queue typed number
+    // example: if i put the first element as a string. it will be a queue typed string
+    private queue: T[];
     private length: number; // number of elements currently in the queue
     private readonly maxSize: number; // maximum number of elements queue can contain
 
@@ -7,29 +9,51 @@ export class Queue {
         // Make sure maxSize is at least 1
         this.maxSize = maxSize > 0 ? maxSize : 10;
         this.length = 0;
+        this.queue = new Array<T>(this.maxSize);
     }
 
-    public isEmpty() {
-
+    public isEmpty(): boolean {
+        return this.length === 0;
     }
 
-    public isFull() {
-        
+    public isFull():boolean {
+       return this.length === this.maxSize; 
     }
     
-    public enqueue() {
-
+    public enqueue(newItem: T): void {
+        if ( this.isFull()){
+            throw new Error("Queue is full")
+        } else {
+            this.queue[this.length++] = newItem // post-increment adds 1 to the length after insertion;
+        }
     }
 
-    public dequeue() {
+    public dequeue(): T { //performance issue 
+        if (this.isEmpty()){
+            throw new Error("Queue is empty");
+        }
 
+        const retval = this.queue[0]
+
+        for(let i = 0; i < this.length; i++){
+            this.queue[i] = this.queue[i+1];
+        }
+
+        this.length--;
+        return retval;
     }
 
-    public peek() {
-
+    public peek(): T {
+        if (this.isEmpty()){
+            throw new Error("Queue is empty");
+        }
+        return this.queue[0]; //return first element;
     }
     
     public queueContents(): void {
-        
+        console.log("Queue Contents");
+        for(let i = 0; i< this.length; i++){
+            console.log(`queue[${i}]:${this.queue[i]}`);
+        }
     }
 }
